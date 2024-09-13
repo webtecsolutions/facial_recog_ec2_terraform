0c03d819180df0e0a6ee8357549b043fb4d1fa26
Amit Duwal <duwalamit@gmail.com>
2024-09-13 12:34:39 +0545

    Add Opensearch SSM retieval to use data

--- modules/ec2/initialconfigure_docker.sh
+++ modules/ec2/initialconfigure_docker.sh
@@ -15,8 +15,16 @@ sudo snap install aws-cli --classic
 sudo usermod -a -G docker $USER
 sudo chmod 777 /var/run/docker.sock
 
-export AWS_REGION=$(sudo aws ssm get-parameter --name region --query "Parameter.Value" --output text)
-export AWS_ACCOUNT_ID=$(sudo aws ssm get-parameter --name account_id --query "Parameter.Value" --output text)
+# Create environment variables and add to profile
+echo export AWS_REGION=$(sudo aws ssm get-parameter --name region --query "Parameter.Value" --output text) | sudo tee -a /etc/profile
+echo export AWS_ACCOUNT_ID=$(sudo aws ssm get-parameter --name account_id --query "Parameter.Value" --output text) | sudo tee -a /etc/profile
+echo export OPENSEARCH_HOST=$(sudo aws ssm get-parameter --name opensearch_domain_endpoint --query "Parameter.Value" --output text) | sudo tee -a /etc/profile
+echo export OPENSEARCH_USERNAME=$(sudo aws ssm get-parameter --name opensearch_master_user_name --query "Parameter.Value" --output text) | sudo tee -a /etc/profile
+echo export OPENSEARCH_PASSWORD=$(sudo aws ssm get-parameter --name opensearch_master_user_password --query "Parameter.Value" --output text) | sudo tee -a /etc/profile
+
+
+# Reload profile
+source /etc/profile
 
 echo $AWS_ACCOUNT_ID
 
@@ -26,5 +34,4 @@ sudo aws ecr get-login-password --region $AWS_REGION | docker login --username A
 # Pull and run the docker image
 docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/facial-recognition-repository:latest
 docker run -d -p 8000:8000 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/facial-recognition-repository:latest
-
 
