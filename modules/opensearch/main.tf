610ba5c3e75e34844183226a28060b9b44ae1f75
Amit Duwal <duwalamit@gmail.com>
2024-09-16 08:36:42 +0545

    Add Policy to Opensearch

--- modules/opensearch/main.tf
+++ modules/opensearch/main.tf
@@ -12,6 +12,7 @@ resource "aws_opensearch_domain" "facial_recog_opensearch" {
   }
   domain_endpoint_options {
     enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
   encrypt_at_rest {
     enabled = true
@@ -21,6 +22,7 @@ resource "aws_opensearch_domain" "facial_recog_opensearch" {
     }
     advanced_security_options {
         enabled = true
+        anonymous_auth_enabled = false
         internal_user_database_enabled = true
         master_user_options {
             master_user_password = var.opensearch_master_user_password
